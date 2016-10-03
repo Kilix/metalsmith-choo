0d2c3b11b11173df5a8301c3d65d15b0d0821415
William Castandet <wcastand@gmail.com>
2016-10-03 10:52:32 +0200

    test(): need to ass tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 const assert = require('chai').assert
 describe('metalsmith-choo', () =>  {
- describe('global', () => {
+ describe('need to add test', () => {
    it('should be true', () => assert.equal(true, true))
  })
 })
